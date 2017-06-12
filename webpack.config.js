e350c8b1d0f4f3bcb4a4b3326aab3e39653f5a27
Akila Adithya <akilaa@JKCSWORLD.INT>
2017-06-12 08:48:46 +0530

    add foundation & jquery

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './app/app.jsx',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$':'jquery',
+      'jQuery':'jquery'
+    })
+  ],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
@@ -21,14 +36,14 @@ module.exports = {
   module: {
     loaders: [
       {
-        loader:'babel-loader',
+        loader: 'babel-loader',
         query: {
-          presets: ['react','es2015','stage-0']
+          presets: ['react', 'es2015', 'stage-0']
         },
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/
       }
     ]
   },
-  devtool:'cheap-module-eval-source-map'
+  devtool: 'cheap-module-eval-source-map'
 };
